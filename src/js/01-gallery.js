42d30a540d2dc12c5b82e794d733f5dec21e4695
Agata Wajda <agawaj98@gmail.com>
2023-09-17 12:15:33 +0200

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const items = [];
+
+galleryItems.forEach(item => {
+  const box = document.createElement('li');
+  box.classList.add('gallery__item');
+  const link = document.createElement('a');
+  link.setAttribute('href', item.original);
+  link.classList.add('gallery__link');
+  box.append(link);
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.setAttribute('src', item.preview);
+  image.setAttribute('alt', item.description);
+  link.append(image);
+  items.push(box);
+});
+
+gallery.append(...items);
+
+let modal = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
