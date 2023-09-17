2e7ab2a09c24a042b360a1f52265e326c005a1a2
Agata Wajda <agawaj98@gmail.com>
2023-09-17 23:25:19 +0200

    task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const localStorageKey = "feedback-form-state";
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+
+if (localStorage.getItem(localStorageKey)) {  
+  formData = JSON.parse(localStorage.getItem(localStorageKey));
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+ }
+
+const formUpdate = throttle(() => {
+  formData.email = form.elements.email.value;
+  formData.message = form.elements.message.value;
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+ 
+}, 500);
+
+form.addEventListener('input', formUpdate);
+
+ form.addEventListener("submit", (event) => {
+   event.preventDefault();
+  console.log(form.elements.email.value) ;
+  console.log(form.elements.message.value) ;
+  localStorage.removeItem(localStorageKey);
+   form.reset();
+ });
