df4211fd796ea2421cbe38e74df4ca571513cd3c
Agata Wajda <agawaj98@gmail.com>
2023-09-17 19:51:24 +0200

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const localStorageKey = 'videoplayer-current-time';
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const timeUpdate = throttle(data => {
+  localStorage.setItem(localStorageKey, data.seconds);
+}, 1000);
+
+player.on('timeupdate', timeUpdate);
+
+player
+  .setCurrentTime(localStorage.getItem(localStorageKey))
+  .catch(function (error) {
+    console.log(error);
+  });
