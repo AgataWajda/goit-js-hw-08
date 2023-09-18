c71a9a9542c3318836814a605414c30252f69ee9
Agata Wajda <agawaj98@gmail.com>
2023-09-18 22:08:54 +0200

    fixed-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -19,8 +19,23 @@ const timeUpdate = throttle(data => {
 
 player.on('timeupdate', timeUpdate);
 
-player
-  .setCurrentTime(localStorage.getItem(localStorageKey))
-  .catch(function (error) {
-    console.log(error);
+
+
+player.on('loaded' , () => {
+  if (localStorage.getItem(localStorageKey)) {
+   player
+    .setCurrentTime(localStorage.getItem(localStorageKey))
+    .catch(function(error) {
+      switch (error.name) {
+        case 'RangeError':
+            console.log(`the time was less than 0 or greater than the videos duration`) ;
+            break;
+
+        default:
+            console.log(error) ;
+            break;
+    }
   });
+}}) ;
+
+
